136331a9a715bb6677ef9e9692960e9db563f3f4
austpull3 <119237117+austpull3@users.noreply.github.com>
2023-10-16 14:46:27 -0500

    Update testtest.py

--- testtest.py
+++ testtest.py
@@ -1,3 +1,14 @@
 import streamlit as st
 import pyodbc
-st.title('Welcome')
+def main_page():
+    st.title('Rental Equipment Correlation Analysis')    
+def page2():
+    st.title("Exploratory Data Analysis")
+#dict for pages
+page_names_to_funcs = {
+    "Welcome Page": main_page,
+    "Data Exploration and Visualization": page2
+    }
+    #select pages
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
