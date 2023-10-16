375419b61b5ef85673990106ec4c35b90a5b8a09
austpull3 <119237117+austpull3@users.noreply.github.com>
2023-10-16 14:58:11 -0500

    Update testtest.py

--- testtest.py
+++ testtest.py
@@ -10,7 +10,7 @@ def main_page():
     password = 'L+S3jr3fP@*1'
     
     # Create a connection string
-    connection_string = f"DRIVER=SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password}"
+    connection_string = f"DRIVER=ODBC Driver 13 for SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password}"
     
     # Establish a connection
     conn = pyodbc.connect(connection_string)
