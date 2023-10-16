1b4765d0eaabd1b3a69515c886bf79e16af85fe9
austpull3 <119237117+austpull3@users.noreply.github.com>
2023-10-16 14:48:42 -0500

    Update testtest.py

--- testtest.py
+++ testtest.py
@@ -2,6 +2,23 @@ import streamlit as st
 import pyodbc
 def main_page():
     st.title('Rental Equipment Correlation Analysis')    
+    import pandas as pd
+
+    server = 'HOWE-ACCT-SERV\SAGE300CRE'
+    database = 'HoweInc'
+    username = 'sa'
+    password = 'L+S3jr3fP@*1'
+    
+    # Create a connection string
+    connection_string = f"DRIVER=SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password}"
+    
+    # Establish a connection
+    conn = pyodbc.connect(connection_string)
+    
+    cursor = conn.cursor()
+    
+    query = "USE [HoweInc]"
+    result = cursor.execute(query)
 def page2():
     st.title("Exploratory Data Analysis")
 #dict for pages
