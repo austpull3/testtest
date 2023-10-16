717f1c8f45bac7f972ec906603b0eba088ae894c
austpull3 <119237117+austpull3@users.noreply.github.com>
2023-10-16 15:19:33 -0500

    Update testtest.py

--- testtest.py
+++ testtest.py
@@ -4,8 +4,6 @@ def main_page():
     st.title('Rental Equipment Correlation Analysis')    
     import pandas as pd
     # Initialize connection.
-    # Uses st.cache_resource to only run once.
-    @st.cache_resource
     def init_connection():
         return pyodbc.connect(
             "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
