3f32b84d6a8aae1a711451b613b8540090a45291
austpull3 <119237117+austpull3@users.noreply.github.com>
2023-10-16 15:32:10 -0500

    Update testtest.py

--- testtest.py
+++ testtest.py
@@ -6,7 +6,7 @@ def main_page():
     # Initialize connection.
     def init_connection():
         return pyodbc.connect(
-            "DRIVER={MSSQL};SERVER="
+            "DRIVER={Timberline Data};SERVER="
             + st.secrets["server"]
             + ";DATABASE="
             + st.secrets["database"]
