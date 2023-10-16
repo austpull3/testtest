2765e3a7425539841c42c6555ee77ea9aa51341c
austpull3 <119237117+austpull3@users.noreply.github.com>
2023-10-16 15:16:37 -0500

    Update testtest.py

--- testtest.py
+++ testtest.py
@@ -3,6 +3,36 @@ import pyodbc
 def main_page():
     st.title('Rental Equipment Correlation Analysis')    
     import pandas as pd
+    # Initialize connection.
+    # Uses st.cache_resource to only run once.
+    @st.cache_resource
+    def init_connection():
+        return pyodbc.connect(
+            "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+            + st.secrets["server"]
+            + ";DATABASE="
+            + st.secrets["database"]
+            + ";UID="
+            + st.secrets["username"]
+            + ";PWD="
+            + st.secrets["password"]
+        )
+    
+    conn = init_connection()
+    
+    # Perform query.
+    # Uses st.cache_data to only rerun when the query changes or after 10 min.
+    @st.cache_data(ttl=600)
+    def run_query(query):
+        with conn.cursor() as cur:
+            cur.execute(query)
+            return cur.fetchall()
+    
+    rows = run_query("SELECT Job from JCM_MASTER__JOB;")
+    
+    # Print results.
+    for row in rows:
+        st.write(f"{row[0]} has a :{row[1]}:")
 def page2():
     st.title("Exploratory Data Analysis")
 #dict for pages
