51c8e6a9066084de98eec3f7e8267387c02ca0ef
austpull3 <119237117+austpull3@users.noreply.github.com>
2023-10-16 15:09:35 -0500

    Update testtest.py

--- testtest.py
+++ testtest.py
@@ -3,22 +3,6 @@ import pyodbc
 def main_page():
     st.title('Rental Equipment Correlation Analysis')    
     import pandas as pd
-
-    server = 'HOWE-ACCT-SERV\SAGE300CRE'
-    database = 'HoweInc'
-    username = 'sa'
-    password = 'L+S3jr3fP@*1'
-    
-    # Create a connection string
-    connection_string = f"SERVER={server};DATABASE={database};UID={username};PWD={password}"
-    
-    # Establish a connection
-    conn = pyodbc.connect(connection_string)
-    
-    cursor = conn.cursor()
-    
-    query = "USE [HoweInc]"
-    result = cursor.execute(query)
 def page2():
     st.title("Exploratory Data Analysis")
 #dict for pages
