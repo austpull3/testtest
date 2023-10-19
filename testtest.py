6fc4935b10c371d1557c2cd837afcd4843605b83
austpull3 <119237117+austpull3@users.noreply.github.com>
2023-10-19 15:08:39 -0500

    Update testtest.py

--- testtest.py
+++ testtest.py
@@ -3,30 +3,13 @@ import pyodbc
 def main_page():
     st.title('Rental Equipment Correlation Analysis')    
     import pandas as pd
-    def init_connection():
-        return pyodbc.connect(
-            "DRIVER={SQL Server};SERVER="
-            + st.secrets["server"]
-            + ";DATABASE="
-            + st.secrets["database"]
-            + ";UID="
-            + st.secrets["username"]
-            + ";PWD="
-            + st.secrets["password"]
-        )
-    
-    conn = init_connection()
-    
-    def run_query(query):
-        with conn.cursor() as cur:
-            cur.execute(query)
-            return cur.fetchall()
-    
-    rows = run_query("SELECT * from mytable;")
-    
-    # Print results.
-    for row in rows:
-        st.write(f"{row[0]} has a :{row[1]}:")
+    import streamlit as st
+    import numpy as np
+    import pandas as pd
+    from sqlalchemy import create_engine
+    from sklearn.neighbors import NearestNeighbors
+    import pyodbc
+    import pymysql
 def page2():
     st.title("Exploratory Data Analysis")
 #dict for pages
