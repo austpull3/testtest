9e3d07dac8f06475c1f127656159a0daf016a265
austpull3 <119237117+austpull3@users.noreply.github.com>
2023-10-19 14:45:52 -0500

    Update testtest.py

--- testtest.py
+++ testtest.py
@@ -4,12 +4,12 @@ def main_page():
     st.title('Rental Equipment Correlation Analysis')    
     import pandas as pd
     # Initialize connection.
-    server = 'HOWE-ACCT-SERV\SAGE300CRE'
+    server = 'HOWE-ACCT-SERV\SAGE300\Data\HOWE\'
     database = 'HoweInc'
     username = 'sa'
     password = 'L+S3jr3fP@*1'
         # Create a connection string
-    connection_string = f"DRIVER=SQLData;SERVER={server};DATABASE={database};UID={username};PWD={password}"
+    connection_string = f"DRIVER=SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password}"
     
     # Establish a connection
     conn = pyodbc.connect(connection_string)
