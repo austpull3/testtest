6d8e0c6da7bc80e350ca3922fc686b62e8ad797a
austpull3 <119237117+austpull3@users.noreply.github.com>
2023-10-19 14:51:16 -0500

    Update testtest.py

--- testtest.py
+++ testtest.py
@@ -5,11 +5,11 @@ def main_page():
     import pandas as pd
     # Initialize connection.
     server = 'HOWE-ACCT-SERV\SAGE300\Data\HOWE'
-    database = 'HoweInc'
+    database = 'HOWE - LIVE TIMBERLINE'
     username = 'sa'
     password = 'L+S3jr3fP@*1'
         # Create a connection string
-    connection_string = f"DRIVER=ODBC Driver 17 for SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password}"
+    connection_string = f"DRIVER=Timberline Data;SERVER={server};DATABASE={database};UID={username};PWD={password}"
     
     # Establish a connection
     conn = pyodbc.connect(connection_string)
