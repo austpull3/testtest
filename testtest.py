c3859892e22935aefe16047d679390850356c5a7
austpull3 <119237117+austpull3@users.noreply.github.com>
2023-10-24 14:03:25 -0500

    Update testtest.py

--- testtest.py
+++ testtest.py
@@ -16,10 +16,10 @@ username = 'sa'
 password = 'L+S3jr3fP@*1'
     
 # Create a connection string
-connection_string = f"DRIVER={SQL Server};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+#connection_string = f"DRIVER={SQL Server};SERVER={server};DATABASE={database};UID={username};PWD={password}"
     
 # Establish a connection
-conn = pyodbc.connect(connection_string)
+conn = pyodbc.connect("DRIVER={SQL Server};SERVER={server};DATABASE={database};UID={username};PWD={password}")
     
 cursor = conn.cursor()
     
