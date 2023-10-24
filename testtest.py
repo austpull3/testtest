0e3752bdd603e899b30eaaad3e83a7b0d805263e
austpull3 <119237117+austpull3@users.noreply.github.com>
2023-10-24 13:54:25 -0500

    Update testtest.py

--- testtest.py
+++ testtest.py
@@ -10,21 +10,21 @@ def page2():
     st.title("Exploratory Data Analysis")
     st.dataframe()
     
-    server = 'HOWE-ACCT-SERV\SAGE300CRE'
-    database = 'HoweInc'
-    username = 'sa'
-    password = 'L+S3jr3fP@*1'
+server = 'HOWE-ACCT-SERV\SAGE300CRE'
+database = 'HoweInc'
+username = 'sa'
+password = 'L+S3jr3fP@*1'
     
-    # Create a connection string
-    connection_string = f"DRIVER=SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password}"
+# Create a connection string
+connection_string = f"DRIVER=SQL Server;SERVER={server};DATABASE={database};UID={username};PWD={password}"
     
-    # Establish a connection
-    conn = pyodbc.connect(connection_string)
+# Establish a connection
+conn = pyodbc.connect(connection_string)
     
-    cursor = conn.cursor()
+cursor = conn.cursor()
     
-    query = "USE [HoweInc]"
-    result = cursor.execute(query)
+query = "USE [HoweInc]"
+result = cursor.execute(query)
 #dict for pages
 page_names_to_funcs = {
     "Welcome Page": main_page,
